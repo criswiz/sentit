4e86a4fd66b2bc821846381766069a5aabed514b
Samuel Agbenorwu <samuel.agbenorwu@tgholdingsgh.com>
2023-07-03 12:38:26 +0000

    Initial commint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
+import Nav from './auth/Nav'
+import QueryWrapper  from './auth/QueryWrapper'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ 
+  subsets: ['latin'], 
+  weight: ["400", "700"], 
+  variable: "--font-roboto"
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +21,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`mx-4 md:mx-48 xl:mx-96 bg-gray-200 ${roboto.variable}`}>
+        <QueryWrapper>
+          <Nav />
+          {children}
+        </QueryWrapper>
+      </body>
     </html>
   )
 }
